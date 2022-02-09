94592e9531a7deea0316db3200687992e60885f6
Prashant Rawat <prashantraw@flock.com>
2022-02-09 11:19:20 +0530

    add id to sessions

--- contact-app/server/src/main/java/com/example/contacts/model/Sessions.java
+++ contact-app/server/src/main/java/com/example/contacts/model/Sessions.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
 import java.sql.Timestamp;
 
@@ -13,6 +14,7 @@ import java.sql.Timestamp;
 @Getter
 @Setter
 public class Sessions {
+  @Id
   @SerializedName("session_token")
   private String sessionToken;
   @SerializedName("user_id")
